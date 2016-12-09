0c1a4467c2dbaf0c35cfacc6d60acbeebcea7e29
mostafaelsayed2002 <mostafa.elsayed.2002@gmail.com>
2016-12-09 22:31:38 +0200

    Add files via upload

---
+++ mian.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+int x;
+cout<<"Please enter your number: ";
+cin>>x;
+
+for(int i=x;i>0;i--)
+
+{
+for(int x=0;x<i;x++)
+{
+ cout<<"*";
+}
+
+cout<<endl;
+
+}
+
+return 0;
+}
