37339608012f4a93b4d04ec57396c9dde0afe8d3
mostafaelsayed2002 <mostafa.elsayed.2002@gmail.com>
2022-03-01 22:37:53 +0200

    Update Car Follow Light.ino

--- Car Follow Light.ino
+++ Car Follow Light.ino
@@ -1,11 +1,12 @@
 void setup() {
-  Serial.begin(9600);
+  
+//first motor
 pinMode (2,OUTPUT);
 pinMode (4,OUTPUT);
 pinMode (3,OUTPUT);
 pinMode (A0, INPUT);
 
-
+//second motor 
 pinMode (7,OUTPUT);
 pinMode (8,OUTPUT);
 pinMode (9,OUTPUT);
@@ -19,19 +20,8 @@ void loop() {
   digitalWrite(2, LOW);
   analogWrite(3,analogRead(A0)*10);
 
- digitalWrite(7, HIGH);
- digitalWrite(8, LOW);
- analogWrite(9,analogRead(A1)*10);
-
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  analogWrite(9,analogRead(A1)*10);
  
- // Serial.println(analogRead(A0)*3);
-
-
-
-
-
-
-
- 
-
 }
